774be8decce227529daba66684377c9c3eaca3f3
dinhthaison <dinhthaison2000@gmail.com>
2022-08-08 16:33:10 +0700

    commit final

--- app/src/main/java/com/example/infinityenglish/view/activity/ProfileActivity.java
+++ app/src/main/java/com/example/infinityenglish/view/activity/ProfileActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.infinityenglish.R;
 import com.example.infinityenglish.databinding.ActivityProfileBinding;
+import com.example.infinityenglish.databinding.DialogEditInformationBinding;
+import com.example.infinityenglish.databinding.DialogLogoutBinding;
 import com.example.infinityenglish.models.Users;
+import com.example.infinityenglish.util.Const;
+import com.example.infinityenglish.util.Utility;
 import com.example.infinityenglish.view.base.BaseActivity;
 import com.example.infinityenglish.viewmodel.UserViewModel;
 
@@ -37,9 +45,9 @@ public class ProfileActivity extends BaseActivity {
 
         Users users = userViewModel.getCurrentUser();
         String uri;
-        if (users != null){
+        if (users != null) {
             uri = users.getAvatar();
-            String imageUri = uri==null ? String.valueOf(R.drawable.avatar) :uri;
+            String imageUri = uri == null ? String.valueOf(R.drawable.avatar) : uri;
             binding.avatar.setImageURI(Uri.parse(imageUri));
             binding.userName.setText(users.getName());
             binding.email.setText(users.getEmail());
@@ -59,7 +67,30 @@ public class ProfileActivity extends BaseActivity {
         binding.clickEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ChangeInformationActivity.starter(ProfileActivity.this);
+                Dialog dialog = new Dialog(ProfileActivity.this);
+                dialog.getWindow().setBackgroundDrawableResource(R.drawable.round_corner_nocolor);
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setGravity(Gravity.CENTER);
+
+                DialogEditInformationBinding binding1 = DataBindingUtil.inflate(LayoutInflater.from(ProfileActivity.this), R.layout.dialog_edit_information, null, false);
+                dialog.setContentView(binding1.getRoot());
+
+
+                binding1.clickConfirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (users != null) {
+                            String pass = binding1.inputPassword.getText().toString();
+                            if (users.getPassword().equals(pass)) {
+                                ChangeInformationActivity.starter(ProfileActivity.this);
+                            } else {
+                                Utility.Notice.snack(binding.profile, Const.Error.information);
+                            }
+                        }
+                    }
+                });
+
+                dialog.show();
             }
         });
     }
